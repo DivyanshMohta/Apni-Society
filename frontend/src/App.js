1cb13aebe340fe281eacdaa0a0b72b4ccee386f6
DivyanshMohta <divyanshmohta021@gmail.com>
2024-08-12 23:09:10 +0530

    Frontend uploaded

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Startup from './pages/Startup'; // Adjust the path if necessary
+
+const App = () => {
+    return (
+        <div>
+            <Startup />
+        </div>
+    );
+};
+
+export default App;
