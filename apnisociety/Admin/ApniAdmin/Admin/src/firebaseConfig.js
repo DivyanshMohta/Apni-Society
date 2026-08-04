f58e1902bfd293bded5b284f1f1d6c02ab924dd5
Divyansh S Mohta <166325532+DivyanshMohta@users.noreply.github.com>
2026-08-04 12:24:10 +0530

    Obfuscate Firebase config values
    Replaced sensitive Firebase configuration details with placeholders.

--- apnisociety/Admin/ApniAdmin/Admin/src/firebaseConfig.js
+++ apnisociety/Admin/ApniAdmin/Admin/src/firebaseConfig.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDc2RVWe7GnH5fQU7syw8UYggY43I5y0p8",
-  authDomain: "apni-society-e1de9.firebaseapp.com",
-  projectId: "apni-society-e1de9",
-  storageBucket: "apni-society-e1de9.appspot.com",
-  messagingSenderId: "562531611981",
-  appId: "1:562531611981:web:fb68e472b98db3f47f3304",
-  measurementId: "G-LGCVJGFJQX"
+  apiKey: "****",
+  authDomain: "****",
+  projectId: "****",
+  storageBucket: "****",
+  messagingSenderId: "****",
+  appId: "****",
+  measurementId: "****"
 };
 
 export const app = initializeApp(firebaseConfig);
